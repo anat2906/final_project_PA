3e31a90f5d9f70ed9c0152374509e0fb49930cba
dinstamaly <dinstamaly@gmail.com>
2019-03-27 19:23:12 +0600

    configure static files

--- mentor/mentor/settings.py
+++ mentor/mentor/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mentor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mentor',
+        'USER': 'din',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'staticfiles')
